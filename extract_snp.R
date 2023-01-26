a8cb1ad3c7538d3feb9f8b2f7092f1c05444945c
hdg204 <36624710+hdg204@users.noreply.github.com>
2023-01-26 12:28:18 +0000

    Create extract_snp.R

---
+++ extract_snp.R
@@ -0,0 +1,32 @@
+extract_snp=function(chr,bp){
+  # This function just creates a list of file names of bgen files in UKBB. This should be constant across all projects, I think
+  bgen_file=paste("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c",chr,"_b0_v3.bgen",sep='')
+ 
+  sample=read.table("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c1_b0_v3.sample",header=T) #this file has all the samples in it, but there's a dummy line at the start
+  eid=sample$ID_1[2:nrow(sample)]
+  
+  #the rbgen package wants the chromosome in two digit form, e.g. '08'
+  if (chr<10){
+    chr_str=paste('0',chr,sep='')
+  }else{
+    chr_str=as.character(chr)
+  }
+  
+  #this tells rbgen where to look
+  ranges = data.frame(
+    chromosome = chr_str,
+    start = bp,
+    end = bp
+  )
+  data=bgen.load(bgen_file, ranges )
+  
+  if (nrow(data$variants)==0){
+	return('SNP not found')
+  }else{
+  mat=data$data[1,,]
+  genotypes=as.numeric(mat[,2]+2*mat[,3])
+  
+  out=list(snp_data=data.frame(eid,genotypes),variant_info=data$variants)
+  return(out)
+  }
+}
