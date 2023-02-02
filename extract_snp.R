5592d64e2b8e7203b57c758947b0554de01569b4
hdg204 <36624710+hdg204@users.noreply.github.com>
2023-02-02 12:43:43 +0000

    Update extract_snp.R
    Function now handles tri-allelic and omni-allelic snps.

--- extract_snp.R
+++ extract_snp.R
@@ -1,7 +1,7 @@
 extract_snp=function(chr,bp){
   # This function just creates a list of file names of bgen files in UKBB. This should be constant across all projects, I think
   bgen_file=paste("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c",chr,"_b0_v3.bgen",sep='')
- 
+  
   sample=read.table("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c1_b0_v3.sample",header=T) #this file has all the samples in it, but there's a dummy line at the start
   eid=sample$ID_1[2:nrow(sample)]
   
@@ -14,11 +14,11 @@ extract_snp=function(chr,bp){
   
   # SEX CHROMOSOMES BREAK THINGS. I fix.
   if (chr==23){
-	# Some people aren't in the sex chromosome bgen file so using the sample file from chromosome 1 doesn't work
-	sample=read.table("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_cX_b0_v3.sample",header=T)
-	bgen_file="../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_cX_b0_v3.bgen"
-	chr_str='X'
-	eid=sample$ID_1[2:nrow(sample)]
+    # Some people aren't in the sex chromosome bgen file so using the sample file from chromosome 1 doesn't work
+    sample=read.table("../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_cX_b0_v3.sample",header=T)
+    bgen_file="../../mnt/project/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_cX_b0_v3.bgen"
+    chr_str='X'
+    eid=sample$ID_1[2:nrow(sample)]
   }
   
   #this tells rbgen where to look
@@ -29,13 +29,19 @@ extract_snp=function(chr,bp){
   )
   data=bgen.load(bgen_file, ranges )
   
-  if (nrow(data$variants)==0){
-	return('SNP not found')
-  }else{
-  mat=data$data[1,,]
-  genotypes=as.numeric(mat[,2]+2*mat[,3])
+  genotypes=data.frame(eid=eid)
   
-  out=list(snp_data=data.frame(eid,genotypes),variant_info=data$variants)
-  return(out)
+  if (nrow(data$variants)==0){
+    return('SNP not found')
+  }else{
+    for (i in 1:nrow(data$data)){ #nrow data$data is the amount of snps picked up at that bp:chr
+      colname=paste(chr,':',bp,'_',data$variants$allele0[i],'_',data$variants$allele1[i],sep='')
+      mat=data$data[i,,]
+      geno=as.numeric(mat[,2]+2*mat[,3])
+      genotypes[[colname]]=geno
+    }
+    
+    out=list(snp_data=genotypes,variant_info=data$variants)
+    return(out)
   }
 }
